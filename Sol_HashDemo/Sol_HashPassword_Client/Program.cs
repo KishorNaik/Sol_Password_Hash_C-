bc3e318ad41d03cabf741d135fb92a50cd4641e9
Kishor Naik <kishor.naik011.net@outlook.com>
2019-11-08 11:33:14 +0530

    Code Commit

--- Sol_HashDemo/Sol_HashPassword_Client/Program.cs
+++ Sol_HashDemo/Sol_HashPassword_Client/Program.cs
@@ -12,14 +12,22 @@ namespace Sol_HashPassword_Client
 
             Task.Run(async () => {
 
+                // Set Plain Password Text
                 var password = "mak123";
 
+                // Generate Unique Salt as per the byte Range
                 var saltData = await Salt.CreateAsync(ByteRange.byte256);
                 Console.WriteLine(saltData);
 
+                // Generate Hash 
                 var hashData = await Hash.CreateAsync(password, saltData,ByteRange.byte256);
                 Console.WriteLine(hashData);
 
+                // Code Emit
+                // Store Salt and Hash in Database.
+                // Get Salt and Hash based on User Name
+
+                // Validate Password with using salt and Hash
                 var flag = await Hash.ValidateAsync("mak123", saltData, hashData,ByteRange.byte256);
                 Console.WriteLine(flag);
                 
